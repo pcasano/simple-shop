88452e8a56914f35a92bbff5de00582ba5bf85ab
pcasano <pablo.casanovas@pm.me>
2024-03-16 14:27:56 +0100

    add shirt template

--- src/app/shirts-page/shirts-page.component.ts
+++ src/app/shirts-page/shirts-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ItemService } from '../item.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-shirts-page',
@@ -6,5 +8,60 @@ import { Component } from '@angular/core';
   styleUrl: './shirts-page.component.css'
 })
 export class ShirtsPageComponent {
+  
+  constructor(
+    private router: Router,
+    private itemService: ItemService) {}
+
+  shirtModelCounter_1: number = 0;
+  shirtModelCounter_2: number = 0;
+  shirtModelCounter_3: number = 0;
+
+  shirtSize_1 = "";
+  shirtSize_2 = "";
+  shirtSize_3 = "";
+
+  increaseShirtModel1() {
+    this.shirtModelCounter_1++;
+  }
+
+  decreaseShirtModel1() {
+    if(this.shirtModelCounter_1 > 0) {
+      this.shirtModelCounter_1--;
+    }    
+  }
+
+  increaseShirtModel2() {
+    this.shirtModelCounter_2++;
+  }
+
+  decreaseShirtModel2() {
+    if(this.shirtModelCounter_2 > 0) {
+      this.shirtModelCounter_2--;
+    }    
+  }
+
+  increaseShirtModel3() {
+    this.shirtModelCounter_3++;
+  }
+
+  decreaseShirtModel3() {
+    if(this.shirtModelCounter_3 > 0) {
+      this.shirtModelCounter_3--;
+    }    
+  }
+
+  onGoHome() {
+    this.router.navigateByUrl("home");
+  }
+
+  onAddToCart() {
+
+  }
+
+  onEmptyCart() {
+
+  }
+
 
 }
